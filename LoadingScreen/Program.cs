216048ce093e1a5719dc49eb125f9ea787a6fede
akhileusadelard-tech <akhileusadelard@gmail.com>
2026-09-23 14:09:45 +0700

    LoadingScreen

---
+++ LoadingScreen/Program.cs
@@ -0,0 +1,7 @@
+﻿int totalSteps = 5;
+
+for (int step = 1; step <= totalSteps; step++)
+{
+    Console.WriteLine($"Loading... Step {step} of {totalSteps}");
+}
+Console.WriteLine("\nDungeon is ready. Enter now!");
